6061a75261e71b06ac8cf862bb9253e855dc903c
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2010-10-19 22:41:48 +0200

    adding ThunderwolfCore version display if TW Core is in use

--- modules/twAdmin/templates/_bottom.php
+++ modules/twAdmin/templates/_bottom.php
@@ -8,4 +8,4 @@ if ($start_year == $act_year) {
 	$years_period = $start_year.'-'.$act_year;
 }
 ?>
-<div id="layout-bottom" class="layout-row"><div class="layout-box"><?php echo __('Copyright &copy; %years_period% %site_name%. All rights reserved', array('%years_period%' => $years_period, '%site_name%' => twAdmin::getProperty('site'))); ?></div></div> <!-- #layout-bottom -->
+<div id="layout-bottom" class="layout-row"><div class="layout-box"><?php echo __('Copyright &copy; %years_period% %site_name%. All rights reserved', array('%years_period%' => $years_period, '%site_name%' => twAdmin::getProperty('site'))); ?><?php if (defined('THUNDERWOLF_VER') && $sf_user->isAuthenticated()): ?><div style="float:right;">ThunderwolfCore version: <?php echo THUNDERWOLF_VER; ?></div><?php endif; ?></div></div> <!-- #layout-bottom -->
