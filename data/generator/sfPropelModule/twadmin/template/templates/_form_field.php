641ad4cc90d5d92038c2c8a080b8adbb7ab6043d
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2012-06-09 22:47:03 +0200

    better help for textarea

--- data/generator/sfPropelModule/twadmin/template/templates/_form_field.php
+++ data/generator/sfPropelModule/twadmin/template/templates/_form_field.php
@@ -7,13 +7,14 @@
 	[?php echo $form[$name]->renderLabel($label, array('class' => 'control-label')) ?]
 	<div class="controls">
 		[?php echo $form[$name]->render($attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes) ?]
+		[?php $is_textarea = false; if (get_class($form[$name]->getWidget()) == 'sfWidgetFormTextarea') { $is_textarea = true; } ?]
 		[?php if ($form[$name]->hasError()): ?]
-		<span class="help-inline">[?php echo $form[$name]->renderError() ?]</span>
+		<span class="[?php if ($is_textarea): ?]help-block[?php else: ?]help-inline[?php endif; ?]">[?php echo $form[$name]->renderError() ?]</span>
 		[?php else: ?]
 			[?php if ($help): ?]
-		<span class="help-inline">[?php echo __($help, array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</span>
+		<span class="[?php if ($is_textarea): ?]help-block[?php else: ?]help-inline[?php endif; ?]">[?php echo __($help, array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</span>
 			[?php elseif ($help = $form[$name]->renderHelp()): ?]
-		<span class="help-inline">[?php echo $help ?]</span>
+		<span class="[?php if ($is_textarea): ?]help-block[?php else: ?]help-inline[?php endif; ?]">[?php echo $help ?]</span>
 			[?php endif; ?]
 		[?php endif; ?]
 	</div>
