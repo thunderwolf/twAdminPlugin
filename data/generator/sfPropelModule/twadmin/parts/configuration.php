85a470f3cb100ae7d3f694fbbe67f19cde5ea603
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2012-08-23 22:32:50 +0200

    problem with svn

--- data/generator/sfPropelModule/twadmin/parts/configuration.php
+++ data/generator/sfPropelModule/twadmin/parts/configuration.php
@@ -6,7 +6,6 @@
  * @package    ##PROJECT_NAME##
  * @subpackage <?php echo $this->getModuleName() . "\n" ?>
  * @author     ##AUTHOR_NAME##
- * @version    SVN: $Id: configuration.php 3534 2012-05-01 15:37:47Z ldath $
  */
 abstract class Base<?php echo ucfirst($this->getModuleName()) ?>GeneratorConfiguration extends sfModelGeneratorConfiguration
 {
