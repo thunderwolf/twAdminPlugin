58ba97ec2c4d14049d58527a4da294fd1dcc9f09
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2012-08-24 08:25:48 +0200

    adding bootsrap 2.1.0 and corrections in navbar

--- modules/twAdmin/templates/_menu.php
+++ modules/twAdmin/templates/_menu.php
@@ -15,49 +15,51 @@ $submenu = $sf_data->getRaw('submenu');
 			</a>
 			<a class="brand" href="<?php echo url_for('@homepage') ?>"><?php echo twAdmin::getProperty('site'); ?></a>
 			<?php if ($sf_user->isAuthenticated() || !empty($ms) || !empty($ts)) : ?>
-			<ul class="nav pull-right">
-				<li class="divider-vertical"></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo __('Select module') ?> <b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<?php foreach ($module as $titem) : ?>
-						<li><a href="<?php echo url_for($titem['url']) ?>"><?php echo __($titem['label'], array(), 'messages') ?></a></li>
-						<?php endforeach; ?>
-					</ul>
-				</li>
-				<li class="divider-vertical"></li>
-				<?php if ($sf_user->isAuthenticated() && twAdmin::getProperty('logout', false)) : ?>
-				<li>
-					<div class="btn-group pull-right">
-						<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-						<i class="icon-user"></i> <?php echo $sf_user->getUsername() ?>
-							<span class="caret"></span>
-						</a>
+			<div class="nav-collapse collapse">
+				<ul class="nav pull-right">
+					<li class="divider-vertical"></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo __('Select module') ?> <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="#">Profile</a></li>
-							<li class="divider"></li>
-							<li><a href="<?php echo url_for(twAdmin::getProperty('logout_route')) ?>">Sign Out</a></li>
+							<?php foreach ($module as $titem) : ?>
+						<li><a href="<?php echo url_for($titem['url']) ?>"><?php echo __($titem['label'], array(), 'messages') ?></a></li>
+							<?php endforeach; ?>
 						</ul>
-					</div>
-				</li>
-				<?php else : ?>
-				<li><a href="#">You are logged as <strong><?php echo $sf_user->getUsername() ?></strong></a></li>
-				<?php endif; ?>
-				<?php if (!$sf_user->isAuthenticated() && twAdmin::getProperty('login', false)) : ?>
-				<li><a href="<?php echo url_for(twAdmin::getProperty('login_route')) ?>"">Sign In</a></li>
-				<?php endif; ?>
-			</ul>
-			
-			<?php if ($sf_user->isAuthenticated() || !empty($menu)) : ?>
-			<div class="nav-collapse">
+					</li>
+					<li class="divider-vertical"></li>
+					<?php if ($sf_user->isAuthenticated() && twAdmin::getProperty('logout', false)) : ?>
+					<li>
+						<div class="btn-group pull-right">
+							<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+							<i class="icon-user"></i> <?php echo $sf_user->getUsername() ?>
+								<span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Profile</a></li>
+								<li class="divider"></li>
+								<li><a href="<?php echo url_for(twAdmin::getProperty('logout_route')) ?>">Sign Out</a></li>
+							</ul>
+						</div>
+					</li>
+					<?php else : ?>
+					<li><a href="#">You are logged as <strong><?php echo $sf_user->getUsername() ?></strong></a></li>
+					<?php endif; ?>
+					<?php if (!$sf_user->isAuthenticated() && twAdmin::getProperty('login', false)) : ?>
+					<li><a href="<?php echo url_for(twAdmin::getProperty('login_route')) ?>"">Sign In</a></li>
+					<?php endif; ?>
+				</ul>
+				<?php if ($sf_user->isAuthenticated() || !empty($menu)) : ?>
 				<ul class="nav">
 					<?php foreach ($menu as $item) : ?>
-					<li<?php if ($item['select'] === true) : ?> class="active"<?php endif; ?>><a href="<?php echo url_for($item['url']) ?>"><?php echo __(
-					$item['label'], array(), 'messages') ?></a></li>
+					<li<?php if ($item['select'] === true) : ?> class="active"<?php endif; ?>>
+						<a href="<?php echo url_for($item['url']) ?>">
+							<?php echo __($item['label'], array(), 'messages') ?>
+						</a>
+					</li>
 					<?php endforeach; ?>
 				</ul>
-			</div><!--/.nav-collapse -->
-			<?php endif; ?>
+				<?php endif; ?>
+			</div>
 			<?php endif; ?>
 		</div>
 	</div>
