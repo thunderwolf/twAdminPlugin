b78fa3d09a3f071cd4e879b4f2a8992cf738d9d9
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-03-06 00:03:20 +0100

    forgotten twadmin15

--- data/generator/sfPropelModule/twadmin15/template/templates/indexSuccess.php
+++ data/generator/sfPropelModule/twadmin15/template/templates/indexSuccess.php
@@ -1,28 +1,37 @@
 [?php use_helper('I18N', 'Date') ?]
 [?php include_partial('<?php echo $this->getModuleName() ?>/assets') ?]
+<?php $template_span = $this->configuration->getValue('template.span'); ?>
 
-<div class="span<?php echo $this->configuration->getValue('template.span') ?>" id="content">
+<div<?php if ($template_span): ?> class="span<?php echo $template_span ?>"<?php endif; ?> id="content">
 	<h1>[?php echo <?php echo $this->getI18NString('list.title') ?> ?]</h1>
 
 	[?php include_partial('<?php echo $this->getModuleName() ?>/flashes') ?]
-
+	<?php if ($template_span): ?>
 	<div class="row-fluid">
 		<div class="span12">
-
+	<?php endif; ?>
 			<?php if ($this->configuration->hasFilterForm()): ?>
+			<?php if ($template_span): ?>
 			<div class="row-fluid">
 				<div class="span12">
+			<?php endif; ?>
 					[?php include_partial('<?php echo $this->getModuleName() ?>/filters', array('form' => $filters, 'configuration' => $configuration, 'helper' => $helper)) ?]
+			<?php if ($template_span): ?>
 				</div>
 			</div>
 			<?php endif; ?>
-
+			<?php endif; ?>
+			<?php if ($template_span): ?>
 			<div class="row-fluid">
+			<?php endif; ?>
 				[?php include_partial('<?php echo $this->getModuleName() ?>/list_header', array('pager' => $pager)) ?]
+			<?php if ($template_span): ?>
 			</div>
-
+			<?php endif; ?>
+			<?php if ($template_span): ?>
 			<div class="row-fluid">
 				<div class="span12">
+			<?php endif; ?>
 					<?php if ($this->configuration->getValue('list.batch_actions')): ?>
 					<form class="form-inline" action="[?php echo url_for('<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'batch')) ?]" method="post">
 					<?php endif; ?>
@@ -36,14 +45,20 @@
 					<?php if ($this->configuration->getValue('list.batch_actions')): ?>
 					</form>
 					<?php endif; ?>
+			<?php if ($template_span): ?>
 				</div>
 			</div>
-
+			<?php endif; ?>
+			<?php if ($template_span): ?>
 			<div class="row-fluid">
+			<?php endif; ?>
 				[?php include_partial('<?php echo $this->getModuleName() ?>/list_footer', array('pager' => $pager)) ?]
+			<?php if ($template_span): ?>
 			</div>
+			<?php endif; ?>
 
-
+	<?php if ($template_span): ?>
 		</div> <!-- <div class="span12"> -->
 	</div> <!-- <div class="row-fluid"> -->
+	<?php endif; ?>
 </div> <!-- <div id="content"> -->
