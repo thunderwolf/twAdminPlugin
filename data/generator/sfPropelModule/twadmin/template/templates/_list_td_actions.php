7d104db976fc8b88ea302e9026074a04badbc476
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2014-01-04 18:15:36 +0100

    fast solve form td acrions

--- data/generator/sfPropelModule/twadmin/template/templates/_list_td_actions.php
+++ data/generator/sfPropelModule/twadmin/template/templates/_list_td_actions.php
@@ -1,16 +1,19 @@
 <td>
-	<div class="btn-group pull-right">
+    <div class="btn-group pull-right">
 <?php foreach ($this->configuration->getValue('list.object_actions') as $name => $params): ?>
-<?php if ('_delete' == $name): ?>
-		<?php echo $this->addCredentialCondition('[?php echo $helper->linkToDelete($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
-
-<?php elseif ('_edit' == $name): ?>
-		<?php echo $this->addCredentialCondition('[?php echo $helper->linkToEdit($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
-
-<?php else: ?>
-	<?php if (!isset($params['size'])) $params['size'] = 'btn-small'; ?>
-		<?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params) ?>
-<?php endif; ?>
+    <?php if ('_delete' == $name): ?>
+        <?php echo $this->addCredentialCondition('[?php echo $helper->linkToDelete($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
+    <?php elseif ('_edit' == $name): ?>
+        <?php echo $this->addCredentialCondition('[?php echo $helper->linkToEdit($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
+    <?php else: ?>
+        <?php
+        $params['params']['class'] = 'btn';
+        if (!isset($params['params']['size'])) {
+            $params['params']['class'] .= ' btn-small';
+        }
+        ?>
+        <?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params) ?>
+    <?php endif; ?>
 <?php endforeach; ?>
-	</div>
+    </div>
 </td>
--- data/generator/sfPropelModule/twadmin15/template/templates/_list_td_actions.php
+++ data/generator/sfPropelModule/twadmin15/template/templates/_list_td_actions.php
@@ -1,22 +1,23 @@
 <td>
-	<div class="btn-group pull-right">
+    <div class="btn-group pull-right">
 <?php foreach ($this->configuration->getValue('list.object_actions') as $name => $params): ?>
-<?php if ('_delete' == $name): ?>
-		<?php echo $this->addCredentialCondition('[?php echo $helper->linkToDelete($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
-
-<?php elseif ('_edit' == $name): ?>
-		<?php echo $this->addCredentialCondition('[?php echo $helper->linkToEdit($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
-
-<?php elseif ('_move_up' == $name) : ?>
-		<?php echo $this->addCredentialCondition('[?php echo $helper->linkToMoveUp($' . $this->getSingularName() . ', ' . $this->asPhp($params) . ', \'btn-small\') ?]', $params) ?>
-
-<?php elseif ('_move_down' == $name) : ?>
-		<?php echo $this->addCredentialCondition('[?php echo $helper->linkToMoveDown($' . $this->getSingularName() . ', ' . $this->asPhp($params) . ', \'btn-small\') ?]', $params) ?>
-
-<?php else: ?>
-	<?php if (!isset($params['size'])) $params['size'] = 'btn-small'; ?>
-		<?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params) ?>
-<?php endif; ?>
+    <?php if ('_delete' == $name): ?>
+        <?php echo $this->addCredentialCondition('[?php echo $helper->linkToDelete($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
+    <?php elseif ('_edit' == $name): ?>
+        <?php echo $this->addCredentialCondition('[?php echo $helper->linkToEdit($'.$this->getSingularName().', '.$this->asPhp($params).') ?]', $params) ?>
+    <?php elseif ('_move_up' == $name) : ?>
+        <?php echo $this->addCredentialCondition('[?php echo $helper->linkToMoveUp($' . $this->getSingularName() . ', ' . $this->asPhp($params) . ', \'btn-small\') ?]', $params) ?>
+    <?php elseif ('_move_down' == $name) : ?>
+        <?php echo $this->addCredentialCondition('[?php echo $helper->linkToMoveDown($' . $this->getSingularName() . ', ' . $this->asPhp($params) . ', \'btn-small\') ?]', $params) ?>
+    <?php else: ?>
+    <?php
+        $params['params']['class'] = 'btn';
+        if (!isset($params['params']['size'])) {
+            $params['params']['class'] .= ' btn-small';
+        }
+    ?>
+        <?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params) ?>
+    <?php endif; ?>
 <?php endforeach; ?>
-	</div>
+    </div>
 </td>
